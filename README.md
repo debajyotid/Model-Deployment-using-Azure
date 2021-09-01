95774441fe2bc60b059f0bd6f70101b61597f50e
Debajyoti Das <31445120+debajyotid@users.noreply.github.com>
2021-09-01 15:42:28 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# AzureDeployment
+# Azure Deployment Demo
+### This project is forked from https://github.com/krishnaik06/AzureDeployment.
+#### I have added more comments and have updated the below Readme to make the files more readable & the overall process more understandable.
+
+In this demonstration, we show how we can deploy a ML model using Azure using a Flask WebAPI.
--- app.py
+++ app.py
@@ -2,10 +2,8 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template, url_for
 import pickle
 
-
 app = Flask(__name__)
 model = pickle.load(open('randomForestRegressor.pkl','rb'))
-
 
 @app.route('/')
 def home():
@@ -26,15 +24,13 @@ def predict():
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     '''
-    For direct API calls trought request
+    For direct API calls through request
     '''
     data = request.get_json(force=True)
     prediction = model.predict([np.array(list(data.values()))])
 
     output = prediction[0]
     return jsonify(output)
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
